9134fd1648951b697eb4eb8e99c854483193402b
DaddyStephen <thakarekunal279@gmail.com>
2021-05-12 19:21:39 +0530

    1
    1

---
+++ javascript/javascript.js
@@ -0,0 +1,42 @@
+function myF1(p1)
+{
+    if (p1==1){
+    document.getElementById("dc1").style.display = "block";
+    }
+    else if(p1==2){
+    document.getElementById("dc2").style.display = "block";
+    }
+    else if(p1==3){
+    document.getElementById("dc3").style.display = "block";
+    }
+    else if(p1==4){
+    document.getElementById("dc4").style.display = "block";
+    }
+    else if(p1==5){
+    document.getElementById("dc5").style.display = "block";
+    }
+    else if(p1==6){
+    document.getElementById("dc6").style.display = "block";
+    }
+}
+function myF2(p1)
+{
+    if (p1==1){
+    document.getElementById("dc1").style.display = "none";
+    }
+    else if(p1==2){
+    document.getElementById("dc2").style.display = "none";
+    }
+    else if(p1==3){
+    document.getElementById("dc3").style.display = "none";
+    }
+    else if(p1==4){
+    document.getElementById("dc4").style.display = "none";
+    }
+    else if(p1==5){
+    document.getElementById("dc5").style.display = "none";
+    }
+    else if(p1==6){
+    document.getElementById("dc6").style.display = "none";
+    }
+}
